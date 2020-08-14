9d5e75c371fc41ad4da3092c711c58c8817c961e
WadaTakafumi <wdtk2525@gmail.com>
2020-08-14 13:58:00 +0900

    change enemy size

--- Assets/Scripts/MugenSeieiScript.cs
+++ Assets/Scripts/MugenSeieiScript.cs
@@ -8,7 +8,7 @@ public class MugenSeieiScript : MonoBehaviour
     void OnTriggerEnter(Collider col)//透明な壁とPlayerが貫通したときに実行
     {
         if(col.gameObject.tag=="Player"){
-            int i=Random.Range(0,3);//0以上3未満のint(整数)をrandに代入→0or1or2
+            int i=Random.Range(0,2);//0以上2未満のint(整数)をrandに代入→0or1
             
             Instantiate(ground[i],new Vector3(0,0,this.transform.position.z+ground[i].transform.localScale.z), Quaternion.identity);
             //groundを生成する位置をこのオブジェクトの親の先端と一致させるために、GroundのzのScaleの値(この例だと25)だけ離れた地点に生成している
